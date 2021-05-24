aff4e17e485dd56cf606f7d9b33df1641418f3a3
Tuan Nguyen <tuannguyen20071844@gmail.com>
2021-05-24 17:56:10 -0400

    Changed help room for summer

--- X/gen.js
+++ X/gen.js
@@ -10,8 +10,14 @@ function init() {
 	let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 	let dayCode = ["m", "t", "w", "r", "f"]
 
-	if((day >= 0 && day <4 && hour >= 1 && hour <= 8) || (day == 4 && hour >= 1 && hour <= 3)) {
-		time.innerText = "Current Time Slot: " + days[day] + " " + hour + ":00-" + (hour+1) + ":00 PM"
+	if((day >= 0 && day <4 && hour >= -2 && hour <= 8) || (day == 4 && hour >= -2 && hour <= 3)) {
+		if(hour >= 1) {
+			time.innerText = "Current Time Slot: " + days[day] + " " + hour + ":00-" + (hour+1) + ":00 PM"
+		} else if(hour == 0) {
+			time.innerText = "Current Time Slot: " + days[day] + " 12:00-1:00 PM"
+		} else {
+			time.innerText = "Current Time Slot: " + days[day] + " " + (hour+12) + ":00-" + (hour+13) + ":00 AM"
+		}
 		slot = document.getElementById(dayCode[day] + hour)
 		if(slot.childNodes.length == 0) {
 			ta.innerText = "No TA Assigned to this Time Slot"
