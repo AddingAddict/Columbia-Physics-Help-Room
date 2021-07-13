e8e9218a0bfac9426d500d01ea2aa1fbefaa6880
Tuan Nguyen <tuannguyen20071844@gmail.com>
2021-07-13 16:04:38 -0400

    Change schedule to include Summer B courses

--- B2/gen.js
+++ B2/gen.js
@@ -7,11 +7,15 @@ function init() {
 	let day = date.getDay() - 1
 	let hour = date.getHours() - 12
 
-	let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-	let dayCode = ["m", "t", "w", "r", "f"]
+	let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	let dayCode = ["m", "t", "w", "r", "f", "s"]
 
-	if((day >= 0 && day <4 && hour >= 1 && hour <= 8) || (day == 4 && hour >= 1 && hour <= 3)) {
-		time.innerText = "Current Time Slot: " + days[day] + " " + hour + ":00-" + (hour+1) + ":00 PM"
+	if((day >= 0 && day <5 && hour >= 0 && hour <= 8) || (day == 5 && hour >= 0 && hour <= 3)) {
+		if(hour >= 1) {
+			time.innerText = "Current Time Slot: " + days[day] + " " + hour + ":00-" + (hour+1) + ":00 PM"
+		} else if(hour == 0) {
+			time.innerText = "Current Time Slot: " + days[day] + " 12:00-1:00 PM"
+		}
 		slot = document.getElementById(dayCode[day] + hour)
 		if(slot.childNodes.length == 0) {
 			ta.innerText = "No TA Assigned to this Time Slot"
